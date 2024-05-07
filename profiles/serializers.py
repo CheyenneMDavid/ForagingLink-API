137bddfe3e2bcf0558dbf9fc2905f05798339d14
Chey <ctenmanikdavid@gmail.com>
2024-05-07 06:12:24 +0000

    Added profile serializer and list view

---
+++ profiles/serializers.py
@@ -0,0 +1,34 @@
+"""
+This file has been predominantly copied from the DRF-API walkthrough project
+with Code Institute.
+
+Serializer for the Profile model, serializes Profile instances to and from 
+JSON format.
+"""
+
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Serializes Profile instances to and from JSON format.
+    """
+
+    owner = serializers.ReadOnlyField(source="owner.username")
+
+    class Meta:
+        """
+        Specifies the model and fields that will be serialized.
+        """
+
+        model = Profile
+        fields = [
+            "id",
+            "owner",
+            "created_at",
+            "updated_at",
+            "name",
+            "content",
+            "image",
+        ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+"""
+This file has been predominantly copied from the DRF-API walkthrough project
+with Code Institute.
 
-# Create your views here.
+It defines views for interacting with profiles.
+"""
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
+from .serializers import ProfileSerializer
+
+
+class ProfileList(APIView):
+    """
+    View for listing all of the profiles.
+    """
+
+    def get(self, request):
+        """
+        Retrieves all the profiles and serializes them.
+        """
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
