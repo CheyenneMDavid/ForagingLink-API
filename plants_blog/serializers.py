d0be4a8e81eac8089f8c3b35c4fb91e79a5d5def
Chey <ctenmanikdavid@gmail.com>
2024-05-08 03:52:24 +0000

    Add image validation to plant_blog/serializer

--- plants_blog/serializers.py
+++ plants_blog/serializers.py
@@ -11,9 +11,38 @@ from plants_blog.models import PlantInFocusPost
 class PlantInFocusPostSerializer(serializers.ModelSerializer):
     """
     Serializes PlantInFocusPost instances to and from JSON format.
+    It includes image validation and returns ValidationError messages as
+    strings that include the field_name in a string.
     """
 
     owner = serializers.ReadOnlyField(source="owner.username")
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source="owner.profile.id")
+    profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+
+    def validate_image(self, value, field_name):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError(
+                f"{field_name} size cannot exceed 2MB.",
+            )
+
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                f"{field_name} height cannot exceed 4096 pixels."
+            )
+
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                f"{field_name} width cannot exceed 4096 pixels."
+            )
+
+        return value
+
+    def validate_main_plant_image(self, value):
+        return self.validate_image(value, "Main Plant Image")
+
+    def validate_confusable_plant_image(self, value):
+        return self.validate_image(value, "Confusable Plant Image")
 
     class Meta:
         """
