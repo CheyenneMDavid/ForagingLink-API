594e5b875d8026699467500255e025a8ca4a48c6
Chey <ctenmanikdavid@gmail.com>
2024-05-10 06:03:56 +0000

    Added post creation tests for plants_blog and corresponding README section

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-
+# Foraging Link API
 
 Apps in Project:
 
 Profiles.
 Plants Blog.
+___
+## Tests
+
+### Written Tests
+- Tests for Plants Blog Application:
+  Tests to verify that only an admin user can create a post `plant_in_focus_post` and that a regular user can't.
+  ![Tests for creating posts for the blog](plants_blog/tests.py)
+  ![Test Pass](https://res.cloudinary.com/cheymd/image/upload/v1715320492/forage/Foraging_API_README_images/api_blog_post_test_results_in3np5.png)
+  All Tests Passed.
--- plants_blog/tests.py
+++ plants_blog/tests.py
@@ -1,3 +1,69 @@
-from django.test import TestCase
+"""
+This file has been written through guidance gained from the DRF-API
+walkthrough project with Code Institute and documentation from:
+https://www.django-rest-framework.org
 
-# Create your tests here.
+The file contains test cases for the PlantInFocusPostListView class.
+It verifies that only an admin user can create a post, whilst a normal
+authenticated user is restricted from creating post.
+For more information on writing tests using Django Rest Framework, refer to:
+https://www.django-rest-framework.org/api-guide/testing/
+"""
+
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+
+
+class PlantInFocusPostListViewTests(APITestCase):
+    """
+    Test to ensure that only an admin_user can create a post.
+    """
+
+    def setUp(self):
+        """
+        Creates an instance of the APIClient.
+        Creates an admin_user and a regular_user for their respective
+        abilities to create or not create posts, according to permissions.
+        """
+        self.client = APIClient()
+        self.admin_user = User.objects.create_superuser(
+            username="admin_user",
+            password="admin_password",
+        )
+        self.normal_user = User.objects.create_user(
+            username="regular_user",
+            password="user_password",
+        )
+
+    def test_logged_in_admin_can_create_post(self):
+        """
+        Checks that an admin user can create a post successfully by simulating
+        authentication as an admin user and making a POST request to create a
+        post.
+        Verifies successful creation of post by returning a status of:
+        HTTP_201_CREATED
+        """
+        self.client.force_authenticate(user=self.admin_user)
+        response = self.client.post(
+            "/plant_in_focus_posts/",
+            # Generic plant name "Test Plant" for testing purposes.
+            data={"main_plant_name": "Test Plant"},
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_normal_user_cannot_create_post(self):
+        """
+        Checks that a normal user can't create a post by simulating
+        authentication as a normal user and making a POST request to create a
+        post.
+        Verifies that the regular/normal authenticated user can't create a
+        post by returning a status of: HTTP_403_FORBIDDEN
+        """
+        self.client.force_authenticate(user=self.normal_user)
+        response = self.client.post(
+            "/plant_in_focus_posts/",
+            # Generic plant name "Test Plant" for testing purposes.
+            data={"main_plant_name": "Test Plant"},
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
