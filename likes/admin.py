eca1680f37e2a50b8b8464ff013aca6dbfe76b51
Chey <ctenmanikdavid@gmail.com>
2024-05-15 07:37:48 +0000

    Ammend search field name

--- likes/admin.py
+++ likes/admin.py
@@ -6,6 +6,10 @@ from likes.models import Like
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
+    """
+    Admin class for managing Like instances in the Django admin interface.
+    """
+
     list_display = (
         "id",
         "owner",
@@ -16,6 +20,6 @@ class LikeAdmin(admin.ModelAdmin):
     list_filter = ("created_at", "owner")
     search_fields = (
         "owner__username",
-        "plant_in_focus_post__title",
+        "plant_in_focus_post__main_plant_name",
         "comment__content",
     )
