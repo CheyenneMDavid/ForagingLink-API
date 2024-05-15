51e67d88bc4c770c2beefd6143f585bc529353ef
Chey <ctenmanikdavid@gmail.com>
2024-05-15 13:20:20 +0000

    Corrected the LikeSerializer. Imported the  missing models and corrected querysets for fields

--- likes/serializers.py
+++ likes/serializers.py
@@ -8,12 +8,25 @@ with Code Institute.
 from django.db import IntegrityError
 from rest_framework import serializers
 from likes.models import Like
+from plants_blog.models import PlantInFocusPost
+from comments.models import Comment
 
 
 class LikeSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    plant_in_focus_post = serializers.PrimaryKeyRelatedField(
+        queryset=PlantInFocusPost.objects.all(), required=False
+    )
+    comment = serializers.PrimaryKeyRelatedField(
+        queryset=Comment.objects.all(), required=False
+    )
 
     class Meta:
+        """
+        Meta class to map the Like model to the LikeSerializer and define the
+        fields to be included.
+        """
+
         model = Like
         fields = [
             "id",
@@ -23,15 +36,26 @@ class LikeSerializer(serializers.ModelSerializer):
             "comment",
         ]
 
+    def validate(self, data):
+        """
+        Ensures that a plant_in_focus_post or comment is liked, but not both
+        as a result of the same instance that's created.
+        """
+        if not data.get("plant_in_focus_post") and not data.get("comment"):
+            raise serializers.ValidationError(
+                "Either plant_in_focus_post or comment must be liked."
+            )
+        if data.get("plant_in_focus_post") and data.get("comment"):
+            raise serializers.ValidationError(
+                "Only one of plant_in_focus_post or comment can be liked."
+            )
+        return data
+
     def create(self, validated_data):
         """
-        Creates a new like instance upon receiving valid data.
-        Raises an error if it's a duplicate.
-
-        Changed to setting the owner of the Like to the user who's making the
-        request. This way ensures that the owner field is already populated
-        with the correct information before the Like instance is created in
-        the database.  Less room errors.
+        Creates a new Like instance upon receiving valid data.
+        Sets the owner of the Like to the user making the request.
+        Raises a validation error if it's a duplicate.
         """
         validated_data["owner"] = self.context["request"].user
         try:
