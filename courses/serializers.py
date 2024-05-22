bef47bbe67a7573392420f1f3f0094ca2adde510
Chey <ctenmanikdavid@gmail.com>
2024-05-22 06:19:58 +0000

    Added CourseSerializer

---
+++ courses/serializers.py
@@ -0,0 +1,33 @@
+"""
+This module defines the Course serializers and related functionalities.
+
+"""
+
+from rest_framework import serializers
+from .models import Course
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Course model.
+
+    It's responsible for converting Course model data
+    to anf from JSON format.
+    """
+
+    class Meta:
+        """
+        Metadata for the CourseSerializer.  It specifies the fields
+        to be serialized.
+        """
+
+        model = Course
+        fields = [
+            "id",
+            "season",
+            "title",
+            "date",
+            "description",
+            "location",
+            "max_capacity",
+        ]
