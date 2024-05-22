0d8c75405b0c0e8f34ace1b5e17af12d76e8d008
Chey <ctenmanikdavid@gmail.com>
2024-05-22 08:46:01 +0000

    Added settings.py, project's main urls.py and migration file

--- foraging_api/urls.py
+++ foraging_api/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path("", include("comments.urls")),
     path("", include("likes.urls")),
     path("", include("followers.urls")),
+    path("", include("courses.urls")),
+    path("", include("course_registrations.urls")),
 ]
