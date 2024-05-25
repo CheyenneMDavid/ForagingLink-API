3e418493007ffa3d0d486b25c76f56e1cb359d1e
Chey <ctenmanikdavid@gmail.com>
2024-05-25 17:11:27 +0000

    Refactored profile views to use generics and added additional annotaed fields

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -19,6 +19,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    comments_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context["request"]
@@ -51,4 +54,7 @@ class ProfileSerializer(serializers.ModelSerializer):
             "image",
             "is_owner",
             "following_id",
+            "comments_count",
+            "followers_count",
+            "following_count",
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -2,91 +2,54 @@
 This file has been predominantly copied from the DRF-API walkthrough project
 with Code Institute.
 
-It defines views for interacting with profiles.
+It defines the views for interacting with profiles, including listing profiles
+and retrieving detailed profile information with additional annotated fields.
 """
 
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from foraging_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     """
-    View for listing all of the profiles. Retrieves all the profiles from the
-    database, serializes them for JSON response, allowing for data to be
-    easily handled on the client side.
+    View for listing all profiles. Retrieves all profiles from the database,
+    annotates them with comments count, followers count, and following count,
+    serializes them for JSON response, allowing for data to be easily handled
+    on the client side.
     """
 
-    def get(self, request):
-        """
-        Retrieves all profiles and serializes them. Uses 'many=True' in
-        serializer
-        to handle multiple objects.
-        """
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={"request": request}
-        )
-        return Response(serializer.data)
-
-
-class ProfileDetail(APIView):
-    """
-    A view that handles HTTP requests for profile details.
-    It utilizes the ProfileSerializer to handle serialization of profile data.
-    """
+    queryset = Profile.objects.annotate(
+        comments_count=Count("owner__comment", distinct=True),
+        followers_count=Count("owner__followed", distinct=True),
+        following_count=Count("owner__following", distinct=True),
+    ).order_by("created_at")
 
     serializer_class = ProfileSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = [
+        "comments_count",
+        "followers_count",
+        "following_count",
+        "owner__followed__created_at",
+        "owner__following__created_at",
+    ]
+
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View for retrieving detailed profile information. Annotates the profile
+    with comments count, followers count, and following count, and serializes
+    the data for JSON response. Allows only the owner of the profile to access
+    the detailed information and facilities to update or delete their profile.
+    """
+
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        """
-        Attempts to retrieve a profile by its primary key (pk).
-        If it exists, it is returned.
-        If not found, it raises an HTTP 404 error.
-        """
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        """
-        Retrieves a profile using its primary key and serializes it to a format
-        that can be easily converted to JSON for response.
-        The serialized data is then sent back to the user.
-        """
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={"request": request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        """
-        Updates a profile identified by its primary key.
-        The data from the request is used to update the profile. If the data
-        is valid, the profile is saved.
-        If the data is not valid, an HTTP 400 error is raised along with the
-        error details.
-        """
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={"request": request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        """
-        Deletes a profile identified by its primary key.
-        """
-        profile = self.get_object(pk)
-        profile.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Profile.objects.annotate(
+        comments_count=Count("owner__comment", distinct=True),
+        followers_count=Count("owner__followed", distinct=True),
+        following_count=Count("owner__following", distinct=True),
+    ).order_by("created_at")
+    serializer_class = ProfileSerializer
