855ad52f124635865ea33f8f021a2e6ac8966e40
Chey <ctenmanikdavid@gmail.com>
2024-06-02 03:08:47 +0000

    Configured database and added environment variables for security

--- foraging_api/settings.py
+++ foraging_api/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 if os.path.exists("env.py"):
     import env
@@ -46,22 +48,23 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = "my-app-auth"
 JWT_AUTH_REFRESH_COOKIE = "my-refresh-token"
+JWT_AUTH_SAMESITE = "None"
 
 REST_AUTH_SERIALIZERS = {
     "USER_DETAILS_SERIALIZER": "foraging_api.serializers.CurrentUserSerializer"
 }
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pwxuee7+9p&p3ig#=a53^=wxcfy$^lprg4hl8yrdy8%m)h)s^7"
+# Secret Key stored in environment variable
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG set to development with logic that will set it to True or
+# False depending on production or local when running.
+DEBUG = "DEV" in os.environ
 
 ALLOWED_HOSTS = [
-    "8000-cheyennemda-foraginglin-js7eafopwr9.ws-eu114.gitpod.io",
+    "localhost",
+    "https://foraging-api-5bc654d11954.herokuapp.com/",
 ]
 
 
@@ -83,6 +86,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "dj_rest_auth.registration",
+    "corsheaders",
     "profiles",
     "plants_blog",
     "comments",
@@ -94,6 +98,7 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -102,6 +107,15 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+if "CLIENT_ORIGIN" in os.environ:
+    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "foraging_api.urls"
 
@@ -127,12 +141,22 @@ WSGI_APPLICATION = "foraging_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+
+if "DEV" in os.environ:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ.get(
+                "DATABASE_URL",
+            )
+        )
+    }
 
 
 # Password validation
