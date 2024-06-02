4f8f159633dd1b67ab7de8cbc1a0e173092048d2
Chey <ctenmanikdavid@gmail.com>
2024-06-02 03:58:32 +0000

    Added custom logout view as bug fix for dj-rest-auth

--- foraging_api/urls.py
+++ foraging_api/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route
+from .views import root_route, logout_route
 
 """
 Main project's urls.py with patterns for the apps within it, using the
@@ -28,6 +28,7 @@ urlpatterns = [
     path("", root_route),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path("dj-rest-auth/logout/", logout_route),
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
     path(
         "dj-rest-auth/registration/",
