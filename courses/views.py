5070b5f9b3a6ee6dcabf7ebee6f4b836d9299ee3
Chey <ctenmanikdavid@gmail.com>
2024-06-03 02:59:24 +0000

    Added get_queryset function with custom filtering to CourseList model

--- courses/views.py
+++ courses/views.py
@@ -15,18 +15,49 @@ from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework import generics
 from .models import Course
 from .serializers import CourseSerializer
+from django.utils import timezone
 
 
+# Assistance from StackOverflow pages here:
+# https://stackoverflow.com/questions/9549744/
+# django-correctly-retrieve-data-where-date-and-time-are-greater-than-now
+# The code isn't copied but the understanding was grasped and tailored using
+# the docs here: https://docs.djangoproject.com/en/5.0/topics/db/queries/
 class CourseList(generics.ListAPIView):
     """
     View to retrieve instances in a read-only format.
     Inherits from "ListAPIView", allowing any user, authenticated or not, to
     read the content.
+
+    The queryset is handled by a function that fetches instances of the
+    courses, and filters them so they only include upcoming courses with dates
+    that are greater than now, ordering them by date in ascending order, and
+    limits the results to the top three.
     """
 
-    queryset = Course.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        now = timezone.now()
+        return Course.objects.filter(date__gt=now).order_by("date")[:3]
+
+    """
+    View to retrieve instances in a read-only format.
+    Inherits from "ListAPIView", allowing any user, authenticated or not, to
+    read the content.
+
+    queryset is handled by a function that fetches instances of courses and
+    filters them so that only the three newest are returned and that are
+    listed in descending order
+    """
+
+    serializer_class = CourseSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        now = timezone.now()
+        return Course.objects.filter(date__gte=now).order_by("date")[:3]
 
 
 class CourseCreate(generics.CreateAPIView):
