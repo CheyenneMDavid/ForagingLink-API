49711f42d7ad681ae1af7124c4c0abe12468b8aa
Cheyenne David <ctenmanikdavid@gmail.com>
2024-09-17 06:33:09 +0000

    comments/serializers.py

--- comments/serializers.py
+++ comments/serializers.py
@@ -19,27 +19,46 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     """
     Serializer for the Comment model handles the conversion of comments
-    to and from JSON format.
+    to and from JSON format.  Using 'naturaltime' for a more user friendly
+    format on the timestamps.
     """
 
+    # Ensure the owner is read only.
     owner = serializers.ReadOnlyField(source="owner.username")
+    # Field for checking if the logged-in user is the owner of the comment
     is_owner = serializers.SerializerMethodField()
+    # Profile ID associated with the comment's owner
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
-    profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    # URL for the profile image.
+    profile_image = serializers.ReadOnlyField(
+        source="owner.profile.image.url"
+    )
+
     created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
     replies = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     replies_count = serializers.ReadOnlyField()
+
+    # Defines a field that can link this comment to another comment as a reply
     replying_comment = serializers.PrimaryKeyRelatedField(
-        queryset=Comment.objects.all(), required=False, allow_null=True
+        # Allows any comment in the database to be referenced as the parent
+        # comment.
+        queryset=Comment.objects.all(),
+        # Makes providing a reply optional, because not all the comments are
+        # replies.
+        required=False,
+        # Permits the field to be null if the comment is not a reply.
+        allow_null=True,
     )
 
     def validate_replying_comment(self, value):
         """
-
-        Ensure that replies don't go more than 2 levels of comments.
+        Ensure that replies don't go more than 2 levels of comments. Level 1
+        being the second because they start at 0.
+        0 is the first level.
         """
 
+        # Nested replies beyond one level one are prevented.
         if value and value.replying_comment:
 
             raise serializers.ValidationError(
@@ -52,6 +71,8 @@ class CommentSerializer(serializers.ModelSerializer):
         """
         Returns "True" if the requesting user is the owner of the comment.
         """
+
+        # Checks if the logged-in user is the owner of the comment.
         request = self.context["request"]
         return request.user == obj.owner
 
@@ -60,6 +81,9 @@ class CommentSerializer(serializers.ModelSerializer):
         Returns time using "naturaltime" from Django's "humanize" module to
         display the timestamp in a more user-friendly way.
         """
+
+        # Converts the creation time of the comment into a more readable
+        # user-friendly format using 'naturaltime'.
         return naturaltime(obj.created_at)
 
     def get_updated_at(self, obj):
@@ -67,6 +91,9 @@ class CommentSerializer(serializers.ModelSerializer):
         Returns time using "naturaltime" from Django's "humanize" module to
         display the timestamp in a more user-friendly way.
         """
+
+        # Converts the time that a comment is updated into a more readable
+        # user-friendly format using 'naturaltime'.
         return naturaltime(obj.updated_at)
 
     class Meta:
