45e9bed9900bc38a0ec0de44bb5f0812f4fffeba
Cheyenne David <ctenmanikdavid@gmail.com>
2024-09-18 06:02:35 +0000

    Added comments to course_registrations/urls.py

--- course_registrations/urls.py
+++ course_registrations/urls.py
@@ -6,15 +6,20 @@ urls for the Course Registrations app.  Using namespaces to avoid conflicts
 with other apps within the project
 """
 
+# URL configurations for the Course Registrations app
+
+# Using namespace for the Course Registrations app URL to avoid URL name clashes.
 app_name = "course_registrations"
 
 urlpatterns = [
     path(
+        # Endpoint for creating a registration
         "create/",
         views.CourseRegistrationCreate.as_view(),
         name="courseregistration_create",
     ),
     path(
+        # Endpoint for handling an individual registration.
         "<int:pk>/",
         views.CourseRegistrationDetail.as_view(),
         name="courseregistration_detail",
