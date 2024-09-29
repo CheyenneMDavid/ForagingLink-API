5a2744abcdc574a15bfde7f46dfdff4208dc303c
Cheyenne David <ctenmanikdavid@gmail.com>
2024-09-29 16:23:48 +0000

    Added comments to profile/models.py

--- profiles/models.py
+++ profiles/models.py
@@ -16,31 +16,43 @@ class Profile(models.Model):
     Represents a user profile and the fields within it.
     """
 
+    # Owner has a one to one relationship with the User model.
     owner = models.OneToOneField(
         User,
+        # Cascade used to the profile is deleted if the user is deleted.
         on_delete=models.CASCADE,
         verbose_name="Owner",
         help_text="The user this profile is for.",
     )
+    # Automatically sets the date and time of when a profile is created.
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name="Created At",
         help_text="When the profile was created.",
     )
+    # Automatically sets the date and time a profile is updated.
     updated_at = models.DateTimeField(
         auto_now=True,
         verbose_name="Updated At",
         help_text="When the profile was last updated.",
     )
+    # Stores the full name of the user, but can be left blank upon creation
+    # and optionally filled out by user at a later date.
     name = models.CharField(
         max_length=255,
         blank=True,
         verbose_name="Name",
         help_text="The user’s full name.",
     )
+    # Initially left blank upon creation, can be optionally filled ou by user
+    # at a later date, providing chosen information about themselves.
     content = models.TextField(
-        blank=True, verbose_name="Content", help_text="About the user."
+        blank=True,
+        verbose_name="Content",
+        help_text="About the user.",
     )
+    # Stores the user's profile image. Uses a default image if one isn't
+    # uploaded
     image = models.ImageField(
         upload_to="images/",
         default="../default_profile_pic_ciw1he.jpg",
@@ -69,6 +81,8 @@ def create_profile(sender, instance, created, **kwargs):
     Signal receiver function for creating a Profile instance automatically
     when a new User instance is created.
     """
+
+    # Automatically creates a profile when a new user is registered.
     if created:
         Profile.objects.create(owner=instance)
 
