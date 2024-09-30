e82a1dfe3f806fdad624d1e025742b1ccc75ba72
Cheyenne David <ctenmanikdavid@gmail.com>
2024-09-30 02:43:12 +0000

    Added comments to profiles/urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -8,12 +8,15 @@ apps within the project
 
 app_name = "profiles"
 
+
 urlpatterns = [
+    # URL for listing all profiles
     path(
         "",
         views.ProfileList.as_view(),
         name="profile_list",
     ),
+    # URL for retrieving, updating, or deleting a specific profile by its ID (pk)
     path(
         "<int:pk>/",
         views.ProfileDetail.as_view(),
