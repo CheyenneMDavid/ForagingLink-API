0c87b906cd1f07109ab310459788e807dcf38606
Cheyenne David <ctenmanikdavid@gmail.com>
2024-11-07 04:10:47 +0000

    Removed duplicate CORS configuration to prevent conflicts in settings.py

--- foraging_api/settings.py
+++ foraging_api/settings.py
@@ -137,16 +137,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-# CORS allowed origins for production site.
-if "CLIENT_ORIGIN" in os.environ:
-    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
-else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
-    ]
-
-CORS_ALLOW_CREDENTIALS = True
-
 
 ROOT_URLCONF = "foraging_api.urls"
 
