6e93b8c011a7e5a0775c21b9f0b640863f113ed9
Cheyenne David <ctenmanikdavid@gmail.com>
2024-11-17 13:58:36 +0000

    Fixed URL patterns to align with front-end structure

--- plants_blog/urls.py
+++ plants_blog/urls.py
@@ -2,28 +2,34 @@ from django.urls import path
 from plants_blog import views
 
 """
-URLs for the Plants Blog app. Using namespaces to avoid conflicts with other
+URLs for the Plants Blog API. Using namespaces to avoid conflicts with other
 apps within the project.
+
+The current paths are not utilized by the front end, as blog posts are
+managed through the admin panel. These paths are included for future
+development, where users with higher permissions than regular authenticated
+users (but not necessarily full admins) will be able to access a screen to
+create and edit blog posts.
 """
 
 app_name = "plants_blog"
 
 urlpatterns = [
+    # URL for viewing the list of PlantInFocus posts.
     path(
-        # URL for viewing the list of PlantInFocus posts.
-        "",
+        "posts/",
         views.PlantInFocusPostList.as_view(),
         name="plant_posts",
     ),
+    # URL for creating a new PlantInFocus post (restricted to admins).
     path(
-        # URL for creating a new PlantInFocus post (restricted to admins).
-        "create/",
+        "posts/create/",
         views.PlantInFocusPostCreate.as_view(),
         name="plant_post_create",
     ),
+    # URL for viewing, updating, or deleting a specific PlantInFocus post.
     path(
-        # URL for viewing, updating, or deleting a specific PlantInFocus post.
-        "<int:pk>/",
+        "posts/<int:pk>/",
         views.PlantInFocusPostDetail.as_view(),
         name="plant_post_detail",
     ),
