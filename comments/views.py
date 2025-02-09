240662a0803a62f7d3b76540baa2b7ebab598008
Cheyenne David <ctenmanikdavid@gmail.com>
2025-02-09 15:40:34 +0000

    Added ProfileCommentsList, removed redundant comments and refined docstrings

--- comments/views.py
+++ comments/views.py
@@ -1,22 +1,15 @@
 """
 This module defines the Comment views and related functionalities.
-It uses the DjangoFilterBackend to enable better filtering capabilities
-in the API, allowing users to filter comments based on specific criteria.
+It enables filtering and searching for comments whilst ensuring
+proper permissions handling.
 
-The "ListCreateAPIView" is for listing and creating comments, with permissions
-set to "IsAuthenticated", ensuring only authenticated users can view and
-create comments. The "RetrieveUpdateDestroyAPIView" is used for retrieving,
-updating, or deleting comments with permissions set to "IsOwnerOrReadOnly",
-ensuring only the owners of comments can update or delete them.
-Unauthenticated users cannot interact with comments unless
-they sign up.
-
-Much of the code in this file is copied from the DRF-API walkthrough
-project with Code Institute and adapted for my own requirements.
+ListCreateAPIView: Allows listing and creating comments.
+RetrieveUpdateDestroyAPIView: Allows viewing, updating, and deleting comments.
+ListAPIView: Retrieves comment replies and comments by specific users.
 """
 
 from django.db.models import Count
-from rest_framework import generics, permissions, filters, serializers
+from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from foraging_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
@@ -25,81 +18,52 @@ from .serializers import CommentSerializer, CommentDetailSerializer
 
 class CommentList(generics.ListCreateAPIView):
     """
-    This view inherits from "ListCreateAPIView", a generic view for handling
-    lists of objects. The permission class is set to "IsAuthenticated",
-    allowing only authenticated users to view and create comments.
+    Lists all comments and allows authenticated users to create new comments.
     """
 
     serializer_class = CommentSerializer
-    # Ensures only authenticated users can interact with the comments
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     queryset = Comment.objects.annotate(
-        # Counts the number of replies each comment has.
         replies_count=Count("replies", distinct=True),
         likes_count=Count("likes", distinct=True),
-        # Orders comments starting with the newest first.
     ).order_by("-created_at")
-    # Allows filtering and searching within the comments.
+
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = [
-        # Enables filtering by the associated post.
         "plant_in_focus_post",
-        # Enables filtering by the comment being replied to.
         "replying_comment",
-        # Enables filtering by the username of the comment owner
         "owner__username",
     ]
     search_fields = [
-        # Enables search according to the content of the comment.
         "content",
-        # Enables filtering by the username of the comment owner
         "owner__username",
-        # Enables search by the name of the plant in the post.
         "plant_in_focus_post__main_plant_name",
     ]
 
     def perform_create(self, serializer):
         """
-        Creates a new comment. It sets the currently logged-in user as the
-        owner of the comment.
-
-        If the comment is a reply to another comment, it ensures that replies
-        are not nested beyond that level (i.e., replies to replies are not allowed).
-        If a comment is attempted to be made as a reply to another reply
-        (thus a second-level reply), a ValidationError is raised to maintain
-        a simple and manageable discussion structure.
-
-        Also, if the comment is a reply, it automatically inherits the
-        'plant_in_focus_post' from the parent comment to prevent database errors.
+        Assigns the current user as the owner of the comment.
+        Ensures replies do not exceed two levels deep.
         """
-
         parent_comment = serializer.validated_data.get("replying_comment")
-
         if parent_comment:
             serializer.save(
                 owner=self.request.user,
-                # Inherits "plant_in_focus_post" from the parent comment
                 plant_in_focus_post=parent_comment.plant_in_focus_post,
             )
         else:
             serializer.save(owner=self.request.user)
 
-        print("Comment successfully saved.")
-
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Inherits from "RetrieveUpdateDestroyAPIView".
-    Retrieves a comment, or updates or deletes it by id if you own it, using
-    the permission "IsOwnerOrReadOnly".
+    Allows a comment to be retrieved, updated, or deleted by its owner.
     """
 
-    # Using Generics to ensure that only the owner of the comment is able to
-    # edit or delete
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.annotate(
-        # Adds a reply count to each comment
         replies_count=Count("replies", distinct=True),
         likes_count=Count("likes", distinct=True),
     ).order_by("-created_at")
@@ -107,42 +71,52 @@ class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
 
 class CommentReplyList(generics.ListAPIView):
     """
-    View to list all replies to a specific comment.
+    Lists all replies to a specific comment.
     """
 
     serializer_class = CommentSerializer
 
     def get_queryset(self):
         """
-        Return replies for the comment.
+        Returns replies associated with a specific comment.
         """
-
-        # Extracts comment ID (pk) from the URL
         parent_comment_id = self.kwargs["pk"]
-        # Only returns the replies for the specific comment
         return Comment.objects.filter(replying_comment_id=parent_comment_id)
 
 
 class CommentReplyDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    View to retrieve, update, or delete a specific reply to a comment.
+    Allows a reply to be retrieved, updated, or deleted by its owner.
     """
 
-    # Using Generics to ensure that only the owner of the comment is able to
-    # edit or delete
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
 
     def get_queryset(self):
         """
-        Returns the specific reply for the given comment (pk) and reply
-        (reply_pk).
+        Returns a specific reply to a comment based on primary keys.
         """
-
-        # Primary Key for the parent coment.
         parent_comment_id = self.kwargs["pk"]
-        # Primary Key for the reply comment.
         reply_id = self.kwargs["reply_pk"]
         return Comment.objects.filter(
             replying_comment_id=parent_comment_id, id=reply_id
+        )
+
+
+class ProfileCommentsList(generics.ListAPIView):
+    """
+    Lists all comments made by a specific user, allowing other authenticated
+    users to see the comments made by a profile owner and their related posts.
+    """
+
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        """
+        Returns comments created by the user associated with the given profile ID.
+        """
+        profile_id = self.kwargs.get("profile_id")
+        return Comment.objects.filter(owner__profile__id=profile_id).order_by(
+            "-created_at"
         )
